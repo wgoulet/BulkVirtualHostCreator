f5a8f870820433a85aef5c37730cac5734d3f466
Walter Goulet <wgoulet@gmail.com>
2017-07-12 21:08:46 -0700

    Initial Commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create nginx tls directory
+mkdir /etc/nginx/tls
+
+# Copy EasyOpenSSLCA to /root directory
+cp ./EasyOpenSSLCA /root
---
+++ setup_scantarget.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+import re
+import io
+import sys
+import shutil
+
+for sindex in range(9,20):    
+    # Create virtual ips
+    ifaces = io.open("/etc/network/interfaces","a")
+    entry = unicode(r"""
+auto eth1:{0}
+iface eth1:{0} inet static
+	address 10.10.10.{0}
+        netmask 255.255.255.0
+
+""".format(sindex))
+    ifaces.write(entry)
+    ifaces.close()
+    subprocess.call(["/sbin/ifup","eth1:{0}".format(sindex)])
+
+    # Create certs in /etc/nginx/tls directory
+    subject = "scandemo1{0}.vfidev.com".format(sindex)
+    subprocess.call(["make","clean"],cwd="/root/EasyOpenSSLCA")
+    subprocess.call(["make","client","SUBJECT={0}".format(subject)],cwd="/root/EasyOpenSSLCA")
+    clientcert = io.open("/root/EasyOpenSSLCA/client.pem")
+    subcacert = io.open("/root/EasyOpenSSLCA/subca.pem")
+    cacert = io.open("/root/EasyOpenSSLCA/ca.pem")
+    
+    cc = clientcert.readlines()
+    clientcert.close()
+    sub = subcacert.readlines()
+    subcacert.close()
+    ca = cacert.readlines()
+    cacert.close()
+    
+    with open("/etc/nginx/tls/chain{0}.pem".format(sindex),"wb") as outfile:
+        for l in cc:
+            outfile.write(l)
+        for l in sub:
+            outfile.write(l)
+        for l in ca:
+            outfile.write(l)
+    outfile.close()
+    
+    shutil.copyfile("/root/EasyOpenSSLCA/client.key","/etc/nginx/tls/client{0}.key".format(sindex))
+    
+    # create nginx config files
+    shutil.copyfile("/etc/nginx/sites-available/master",
+    "/etc/nginx/sites-enabled/{0}".format(sindex))
+    
+    with open("/etc/nginx/sites-available/master","r") as infile:
+        with open("/etc/nginx/sites-enabled/{0}".format(sindex),"w") as outfile:
+            for line in infile.readlines():
+                if re.search(r'REPLACEIP',line):
+                    outfile.write(re.sub(r'REPLACEIP',"10.10.10.{0}".format(sindex),line))
+                elif re.search(r'REPLACESUBJECT',line):
+                    outfile.write(re.sub(r'REPLACESUBJECT',subject,line))
+                elif re.search(r'REPLACECERT',line):
+                    outfile.write(re.sub(r'REPLACECERT',"/etc/nginx/tls/chain{0}.pem".format(sindex),
+                    line))
+                elif re.search(r'REPLACEKEY',line):
+                    outfile.write(re.sub(r'REPLACEKEY',"/etc/nginx/tls/client{0}.key".format(sindex),
+                    line))
+                else:
+                    outfile.write(line) 
