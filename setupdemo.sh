119f374899cb328a34a807fb6221fedb4e5c2463
walter.goulet@venafi.com <walter.goulet@venafi.com>
2017-08-11 11:14:17 -0400

    removed local steps to create repo dir

--- setupdemo.sh
+++ setupdemo.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir ~/repos
-cd ~/repos
 git clone https://github.com/wgoulet/EasyOpenSSLCA.git
-git clone https://github.com/wgoulet/BulkVirtualHostCreator.git
 cd ~/repos/BulkVirtualHostCreator
 sudo ./init.sh $1
 # Reboot workstation to persist network setting &
