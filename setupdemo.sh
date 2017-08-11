af2aa40af88ef4ec82a3677da21ee43982b68b34
walter.goulet@venafi.com <walter.goulet@venafi.com>
2017-08-11 11:13:18 -0400

    add network device arg to setupdemo.sh

--- setupdemo.sh
+++ setupdemo.sh
@@ -5,8 +5,7 @@ cd ~/repos
 git clone https://github.com/wgoulet/EasyOpenSSLCA.git
 git clone https://github.com/wgoulet/BulkVirtualHostCreator.git
 cd ~/repos/BulkVirtualHostCreator
-chmod +x init.sh
-sudo ./init.sh
+sudo ./init.sh $1
 # Reboot workstation to persist network setting &
 # disable NetworkManager
 echo "Restarting system in 5 seconds; when system is back up execute ./finishdemo.sh"
